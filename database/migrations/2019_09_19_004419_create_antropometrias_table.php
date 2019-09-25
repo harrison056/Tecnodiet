ed604623bf13178c024e0fc60d4102ba14be911a
harrison056 <costa.silva.cs.cs@gmail.com>
2019-09-25 14:20:15 -0300

    Atualização da tabela'

--- database/migrations/2019_09_19_004419_create_antropometrias_table.php
+++ database/migrations/2019_09_19_004419_create_antropometrias_table.php
@@ -9,12 +9,40 @@ class CreateAntropometriasTable extends Migration
     /**
      * Run the migrations.
      *
-     * @return void
+     * @return void$table->string('');
+     $table->string('');
+     $table->string('');
+     $table->string('');
      */
     public function up()
     {
         Schema::create('antropometrias', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('altura');
+            $table->string('peso'); 
+            $table->string('dtNascimento');
+            $table->string('bracoDirRelaxado');
+            $table->string('bracoEsqRelaxado');
+            $table->string('bracoDirContraido');
+            $table->string('bracoEsqContraido');
+            $table->string('antebracoDir');
+            $table->string('antebracoEsq');
+            $table->string('punhoDir');
+            $table->string('punhoEsq');
+            $table->string('pescoco');
+            $table->string('ombro');
+            $table->string('peitoral');
+            $table->string('cintura');
+            $table->string('abdomen');
+            $table->string('quadril');
+            $table->string('panturrilhaDir');
+            $table->string('panturrilhaEsq');
+            $table->string('coxaDir');
+            $table->string('coxaEsq');
+
+            $table->integer('paciente_id')->unsigned()->nullable();
+            $table->foreign('paciente_id')->references('id')->on('pacientes');
+
             $table->timestamps();
         });
     }
